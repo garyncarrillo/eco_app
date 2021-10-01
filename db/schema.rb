8f954ed4e6577869abd7efc4c4da6ddf78404e2f
Garyn Carrillo <iwdev@Garyns-MacBook-Pro.local>
2021-10-01 15:07:17 -0500

    Add virtual store ang mananger article on admin profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_30_023908) do
+ActiveRecord::Schema.define(version: 2021_10_01_195948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "name"
+    t.decimal "stock"
+    t.decimal "rate"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
