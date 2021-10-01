827a1610c4652fb19d88869acd8d44a2a450afde
Garyn Carrillo <iwdev@Garyns-MacBook-Pro.local>
2021-09-30 22:02:22 -0500

    Add url of different resources according his/her roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,25 @@ class User < ApplicationRecord
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable
 
-  enum role: [:admin, :user, :mananger]
+  ROLE_ADMIN = 'admin'
+  ROLE_USER = 'user'
+  ROLE_MANANGER = 'mananger'
+
+  enum role: [ROLE_ADMIN, ROLE_USER, ROLE_MANANGER]
 
   validates :email, uniqueness: true
   validates :email, presence: true
   validates :name, presence: true
+
+  def admin?
+    self.role == ROLE_ADMIN
+  end
+
+  def user?
+    self.role == ROLE_USER
+  end
+
+  def mananger?
+    self.role == ROLE_MANANGER
+  end
 end
