262c38cf16164aeed4b6fead1021e2b3864a7679
Garyn Carrillo <iwdev@Garyns-MacBook-Pro.local>
2021-10-14 20:35:51 -0500

    Add responsive on table

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.create(email: 'admin@eco-app.com', name: 'Admin eco app', password: '123456')
+
 Category.create(
   name: 'Carton',
   score: 1,
